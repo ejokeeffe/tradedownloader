71a236fe0f8849359195eed98370d318d7ce4f72
Eoin O'Keeffe <eokeeffe@gmail.com>
2014-10-06 09:08:27 +0100

    test file

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ Created on Fri Oct 03 14:28:25 2014
 @author: Eoin O'Keeffe
 """
 
+import getTradeDataFromComtrade
+
+comapi=getTradeDataFromComtrade.ComtradeApi()
+df=comapi.getComtradeData()
+print df.head
